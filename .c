3d3b01dd4d2c361402949ba17ec855541c5009e7
AndesondaCruz <andersondacruzdealmeida@gmail.com>
2025-09-06 14:19:34 -0300

    Questão 02: q02.c

--- .c
+++ .c
@@ -1,7 +1,12 @@
 #include<stdio.h>
+#include<locale.h>
 int main (int argc, char *argv[]){
-
-    printf("Hello World!");
-
+    setlocale(LC_ALL, "Portuguese");
+    int n1, n2, soma;
+    printf("Digite dois números inteiros: ");
+    scanf("%d %d", &n1, &n2);
+    soma = n1 + n2;
+    printf("A soma dos dois números é: %d", soma);
+   
     return 0;
 }
