430d6c0697970534fb7b90cae6b69f453a2c669f
Andeson da Cruz <andersondacruzdealmeida@gmail.com>
2025-09-13 20:38:18 -0300

    Questão 02: q2.c

--- .c
+++ .c
@@ -1,19 +1,12 @@
 #include<stdio.h>
 #include<locale.h>
-
-int main(){
+int main (int argc, char *argv[]){
     setlocale(LC_ALL, "Portuguese");
-    int vet[10] = {10,20,30,40,50,60,70,80,90,100};
-    float media;
-    int i;
-    int soma = 0;
-
-    for(i=0; i<10; i++){
-        soma = soma + vet[i];
-    }
-        media = soma/10;
-        printf("A média do vetor que armazena 10 números inteiros é %.2f\n", media);
-
-    system("pause");
+    int n1, n2, soma;
+    printf("Digite dois nÃºmeros inteiros: ");
+    scanf("%d %d", &n1, &n2);
+    soma = n1 + n2;
+    printf("A soma dos dois nÃºmeros Ã©: %d", soma);
+   
     return 0;
 }
