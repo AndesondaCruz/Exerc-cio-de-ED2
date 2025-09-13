cba736f2fc1b38bb62102372e64f047e0e49ac04
Andeson da Cruz <andersondacruzdealmeida@gmail.com>
2025-09-13 20:33:15 -0300

    Questão 03: q3.c

--- .c
+++ .c
@@ -1,12 +1,19 @@
 #include<stdio.h>
 #include<locale.h>
-int main (int argc, char *argv[]){
+
+int main(){
     setlocale(LC_ALL, "Portuguese");
-    int n1, n2, soma;
-    printf("Digite dois n√∫meros inteiros: ");
-    scanf("%d %d", &n1, &n2);
-    soma = n1 + n2;
-    printf("A soma dos dois n√∫meros √©: %d", soma);
-   
+    int vet[10] = {10,20,30,40,50,60,70,80,90,100};
+    float media;
+    int i;
+    int soma = 0;
+
+    for(i=0; i<10; i++){
+        soma = soma + vet[i];
+    }
+        media = soma/10;
+        printf("A mÈdia do vetor que armazena 10 n˙meros inteiros È %.2f\n", media);
+
+    system("pause");
     return 0;
 }
