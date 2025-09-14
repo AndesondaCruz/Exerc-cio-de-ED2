7dfcb8aa61f714d4aa134bd1f44a3b1d819d6a39
Andeson da Cruz <andersondacruzdealmeida@gmail.com>
2025-09-13 21:59:33 -0300

    Questão 05; q5.c

---
+++ Questão 05; q5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<locale.h>
+
+int main (){
+    
+    int vet[10] = {1,2,3,4,5,6,7,8,9,10};
+    int i = 10;
+
+    for(i=9; i>=0; i--){
+        printf("%d", vet[i]);
+    }
+     printf("\n");
+    system("pause");
+    return 0;
+}
