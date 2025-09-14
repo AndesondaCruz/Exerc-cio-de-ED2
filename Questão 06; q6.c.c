700bfec40cf7da2afc5abaea46b370a2b8896d78
Andeson da Cruz <andersondacruzdealmeida@gmail.com>
2025-09-14 19:11:07 -0300

    Questão 06; q6.cc

---
+++ Questão 06; q6.c.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<locale.h>
+
+int main (){
+    setlocale(LC_ALL, "Portuguese");
+
+    int vet[10] = {23,43,3,4,50,6,7,8,998,10};
+    int i, pares = 0;
+
+        for(i=0; i<10; i++){
+            if (vet[i] % 2 == 0){
+                pares++;
+            }
+        }
+            printf("Os números inteiros pares no vetor é: %d\n", pares);
+    system("pause");
+    return 0;
+}
