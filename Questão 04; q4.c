fc977f8c905551a3c329b0f76dd2968094503f24
Andeson da Cruz <andersondacruzdealmeida@gmail.com>
2025-09-13 21:45:14 -0300

    Questão 04; q4.c

---
+++ Questão 04; q4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<locale.h>
+
+int main (){
+    setlocale(LC_ALL, "Portuguese");
+    int vet[10] = {1,2,9,40,5,63,17,8,91,10};
+    int i;
+    int maior = vet[0];
+        for(i=1; i<10; i++){
+            if(vet[i] > maior){
+                maior = vet[i];
+            }
+        }
+                printf("O maior elemento no vetor é: %d", maior);
+    return(0);
+    system("pause");
+}
